1638f604e2c0f708bbf8a7eb58a5f280db064b3a
Alexander Pisarev <alexander.a.pisarev@gmail.com>
2020-09-05 12:58:42 +0100

    More market data refactoring

--- utils/helper.py
+++ utils/helper.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+import scipy.optimize as spo
 import os
 import math
 import utils
@@ -9,15 +10,68 @@ import datetime
 import argparse
 
 
-def fill_missing_values(df_data):
-    """Fill missing values in data frame, in place."""
-    df_data.fillna(method='ffill', inplace=True)
-    df_data.fillna(method='bfill', inplace=True)
-
-
 def symbol_to_path(symbol, base_dir="data", ext="csv"):
     """Return CSV file path given ticker symbol."""
     return os.path.join(base_dir, "{}.{}".format(str(symbol), str(ext)))
+
+
+class MarketDataParam(object):
+    def __init__(self, dates, param):
+        self.param = param
+        self.df = pd.DataFrame(index=dates)
+
+    def add_snp_baseline(self):
+        # SPY is S&P500 ETF and is used as reference stock.
+        # It should be always added to dataframe.
+        self.add_ticker("SPY")
+        # SPY is traded on every day that exchange is open.
+        # All the missing dates are not interesting for calculations.
+        self.df = self.df.dropna(subset=["SPY"])
+
+    def add_ticker(self, ticker, base_dir="data"):
+        if ticker in self.df.columns:
+            return
+
+        file_path = symbol_to_path(ticker, base_dir)
+        df_temp = pd.read_csv(file_path, parse_dates=True, index_col="Date",
+                              usecols=["Date", self.param], na_values=["nan"])
+        df_temp = df_temp.rename(columns={self.param: ticker})
+        self.df = self.df.join(df_temp)
+
+    def add_tickers(self, tickers, base_dir="data"):
+        for ticker in tickers:
+            self.add_ticker(ticker)
+
+    def fill_missing_values(self):
+        """Fill missing values in data frame, in place."""
+        self.df.fillna(method='ffill', inplace=True)
+        self.df.fillna(method='bfill', inplace=True)
+
+    def normalize(self):
+        """Normalize data by first row"""
+        return self.df / self.df.ix[0]
+
+    def portfolio_val(self, allocates, cost=1.0):
+        port = self.normalize() * allocates
+        port = port * cost
+
+        dr = port.sum(axis=1)
+        dr.name = "Portfolio"
+        return dr.to_frame()
+
+    def fit_line(self, error_func):
+        column_num = self.df.shape[1]
+        init_allocates = np.ones(column_num) / column_num
+        limits = ()
+        for x in range(column_num):
+            limits += ((0.0, 1.0),)
+
+        constr = {'type': 'eq', 'fun': sum_one}
+
+        result = spo.minimize(error_func, init_allocates, args=(self,),
+                              method='SLSQP', bounds=limits,
+                              constraints=constr, options={'disp': True})
+        return result.x
 
 
 class MarketData(object):
@@ -25,50 +79,30 @@ class MarketData(object):
         self.dates = dates
         self.data = {}
 
-    def add_snp_baseline(self, param):
-        # SPY is S&P500 ETF and is used as reference stock.
-        # It should be always added to dataframe.
-        self.add_ticker("SPY", param)
-        # SPY is traded on every day that exchange is open.
-        # All the missing dates are not interesting for calculations.
-        self.data[param] = self.data[param].dropna(subset=["SPY"])
-
     def add_param(self, param):
-        self.data[param] = pd.DataFrame(index=self.dates)
-
-    def add_ticker(self, ticker, param, base_dir="data"):
         if param not in self.data:
-            self.add_param(param)
+            self.data[param] = MarketDataParam(self.dates, param)
 
-        df = self.data[param]
-        if ticker in df.columns:
-            return
-
-        file_path = symbol_to_path(ticker, base_dir)
-        df_temp = pd.read_csv(file_path, parse_dates=True, index_col="Date",
-                              usecols=["Date", param], na_values=["nan"])
-        df_temp = df_temp.rename(columns={param: ticker})
-        self.data[param] = df.join(df_temp)
+        return self.data[param]
 
 
 def get_snp_data(tickers, dates):
     """Read stock data (adjusted close) for given symbols from CSV files."""
     md = MarketData(dates)
-    md.add_snp_baseline("Adj Close")
+    ac_data = md.add_param("Adj Close")
 
-    for ticker in tickers:
-        md.add_ticker(ticker, "Adj Close")
+    ac_data.add_snp_baseline()
+    ac_data.add_tickers(tickers)
 
-    return md.data["Adj Close"]
+    return ac_data.df
 
 
 def get_data(tickers, params, dates, base_dir="data"):
     """Read stock data (adjusted close) for given symbols from CSV files."""
     md = MarketData(dates)
     for param in params:
-        md.add_param(param)
-        for ticker in tickers:
-            md.add_ticker(ticker, param, base_dir)
+        param_data = md.add_param(param)
+        param_data.add_tickers(tickers, base_dir)
     return md.data
 
 
@@ -127,20 +161,6 @@ def compute_prediction(df, predict):
     return dr
 
 
-def normalize(df):
-    """Normalize data by first row"""
-    return df / df.ix[0]
-
-
-def portfolio_val(df, allocates, cost=1.0):
-    port = normalize(df) * allocates
-    port = port * cost
-
-    dr = port.sum(axis=1)
-    dr.name = "Portfolio"
-    return dr.to_frame()
-
-
 def sharpe_ratio(df, daily_free_risk):
     daily_returns = compute_daily_returns(df)
 
@@ -150,6 +170,15 @@ def sharpe_ratio(df, daily_free_risk):
 
 def daily_free_risk():
     return (1.08 ** (1 / 365) - 1.0)
+
+
+def reverse_sr(allocates, mdp):
+    return utils.sharpe_ratio(mdp.portfolio_val(allocates),
+                              daily_free_risk()) * -1
+
+
+def sum_one(allocates):
+    return np.sum(allocates) - 1.0
 
 
 def print_statistic(df, daily_free_risk):
