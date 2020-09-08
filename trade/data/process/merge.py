aa0762d903d5f9e9f69cb1d7255fd7c1e043cb89
Alexander Pisarev <alexander.a.pisarev@gmail.com>
2020-09-08 10:52:16 +0100

    Make merger accept list of stocks

--- trade/data/process/merge.py
+++ trade/data/process/merge.py
@@ -1,16 +1,13 @@
 from trade.data import storage
 
 class Merger(object):
-    def process(self, stock_a, stock_b):
+    def process(self, stocks):
         result_stock = storage.Stock()
 
-        for name, column in stock_a.data.iteritems():
-            result_column = result_stock.column(name)
-            result_column.df = result_column.df.join(column.df, how="outer")
-
-        for name, column in stock_b.data.iteritems():
-            result_column = result_stock.column(name)
-            result_column.df = result_column.df.join(column.df, how="outer")
+        for stock in stocks:
+            for name, column in stock.data.iteritems():
+                result_column = result_stock.column(name)
+                result_column.df = result_column.df.join(column.df, how="outer")
 
         return result_stock
 
